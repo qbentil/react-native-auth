d5b8009a2551ff00fe76cb50d140b4aec1502aff
qbentil <Bentilshadrack72@gmail.com>
2021-09-29 00:27:07 +0000

    Testing Environment

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { Component } from 'react';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+export default class App extends React.Component{
+  render (){
+    return(
+        <View style={styles.container}>
+        <Text style={{color: "#fff"}}>Ready for My React Native Course</Text>
+        <Text style={{color: "#fff"}}>I am BENTIL🤣🤣</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
