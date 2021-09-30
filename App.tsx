34dd43cc56c585cd4450c4455e44f12f7fef029d
qbentil <Bentilshadrack72@gmail.com>
2021-09-30 00:21:37 +0000

    Learning Statea and Props

--- App.tsx
+++ App.tsx
@@ -1,15 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 import { Component } from 'react';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
 export default class App extends React.Component{
+  state = {
+    number: 0
+  }
+  add = () => {
+    this.setState({number: this.state.number +1})
+  }
+  subtract = () => {
+    if((this.state.number-1) < 0)
+    {
+      alert("Minimum Digit should be Zero")
+    }else{
+      this.setState({number: this.state.number -1})
+    }
+  }
+  
   render (){
     return(
         <View style={styles.container}>
-        <Text style={{color: "#fff"}}>Ready for My React Native Course</Text>
-        <Text style={{color: "#fff"}}>I am BENTIL🤣🤣</Text>
+        <Text>Learning Props and States</Text>
+        
+        <Text style ={styles.screen}>{this.state.number}</Text>
+        <TouchableOpacity style = {styles.button} onPress={this.add}>
+          <Text style = {{fontSize: 20}}>Increase</Text>
+        </TouchableOpacity>
+
+        
+        <TouchableOpacity style = {styles.button} onPress={this.subtract}>
+          <Text style = {{fontSize: 20}}>Decrease</Text>
+        </TouchableOpacity>
+
+        
         <StatusBar style="auto" />
       </View>
     )
@@ -19,8 +45,23 @@ export default class App extends React.Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    marginVertical: 10,
+    height: 50,
+    paddingHorizontal: 20,
+    backgroundColor: "#ccc",
+    justifyContent: 'center',
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  screen: {
+    margin: 15,
+    fontSize: 20,
+    color: "green",
+    
+  }
 });
