e549992daf886f8837387452b939db9eb4fa3b6b
qbentil <Bentilshadrack72@gmail.com>
2021-10-02 00:03:10 +0000

    Updating App.jsx

--- App.tsx
+++ App.tsx
@@ -3,41 +3,13 @@ import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import { Component } from 'react';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import SwitchNavigator from './navigation/loginNavigator'
 
 export default class App extends React.Component{
-  state = {
-    number: 0
-  }
-  add = () => {
-    this.setState({number: this.state.number +1})
-  }
-  subtract = () => {
-    if((this.state.number-1) < 0)
-    {
-      alert("Minimum Digit should be Zero")
-    }else{
-      this.setState({number: this.state.number -1})
-    }
-  }
   
   render (){
     return(
-        <View style={styles.container}>
-        <Text>Day3: Conditional Rendering</Text>
-        
-        <Text style ={styles.screen}>{this.state.number}</Text>
-        <TouchableOpacity style = {styles.button} onPress={() => this.add()}>
-          <Text style = {{fontSize: 20}}>Increase</Text>
-        </TouchableOpacity>
-
-        
-        <TouchableOpacity style = {styles.button} onPress={this.subtract}>
-          <Text style = {{fontSize: 20}}>Decrease</Text>
-        </TouchableOpacity>
-
-        
-        <StatusBar style="auto" />
-      </View>
+      <SwitchNavigator /> 
     )
   };
 }
@@ -49,19 +21,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
-    marginVertical: 10,
-    height: 50,
-    paddingHorizontal: 20,
-    backgroundColor: "#ccc",
-    justifyContent: 'center',
-    alignItems: "center",
-    borderRadius: 5,
-  },
-  screen: {
-    margin: 15,
-    fontSize: 20,
-    color: "green",
-    
-  }
+
 });
