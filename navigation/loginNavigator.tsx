a97716eb84519c5621e68daee56dbb64200dcd4c
qbentil <Bentilshadrack72@gmail.com>
2021-10-04 00:16:45 +0000

    Designing Login Page

--- navigation/loginNavigator.tsx
+++ navigation/loginNavigator.tsx
@@ -2,6 +2,7 @@
 
 import * as React from 'react';
 
+import AppLoading from 'expo';
 import Login from '../screens/Auth/Login';
 import { NavigationContainer } from '@react-navigation/native';
 import Signup from '../screens/Auth/Signup';
@@ -10,14 +11,23 @@ import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name = "Login" component = {Login} options = {{headerShown: true}} />
-        <Stack.Screen  name = "Signup" component = {Signup} options = {{headerShown: true}} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  let [fontLoaded] = useFonts({
+    
+  });
+  
+
+  if(fontLoaded){
+    return <AppLoading />
+  }else{
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name = "Login" component = {Login} options = {{headerShown: true}} />
+          <Stack.Screen  name = "Signup" component = {Signup} options = {{headerShown: true}} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 // export default App;
