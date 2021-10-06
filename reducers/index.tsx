930119dc443add500dad7b867cfdde007d3cdfb4
qbentil <Bentilshadrack72@gmail.com>
2021-10-06 00:08:11 +0000

    Updating reducres

--- reducers/index.tsx
+++ reducers/index.tsx
@@ -5,6 +5,10 @@ import { combineReducers } from "redux"
 const user = (state = {}, action) => {
     switch (action.type)
     {
+        case 'UPDATE_EMAIL':
+            return { ...state, email:action.payload}
+        case 'UPDATE_PASSWORD':
+            return { ...state, password:action.payload}
         default: 
             return state;
     }
