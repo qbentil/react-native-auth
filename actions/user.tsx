e7579f9b8d09a0af28183ec82d126fe703ae4f31
qbentil <Bentilshadrack72@gmail.com>
2021-10-06 00:08:31 +0000

    Creating User Functions

---
+++ actions/user.tsx
@@ -0,0 +1,6 @@
+export const updateUsername = (input) => {
+	return {type:'UPDATE_EMAIL', payload: input}
+}
+export const updatePassword = (input) => {
+	return {type:'UPDATE_PASSWORD', payload: input}
+}
